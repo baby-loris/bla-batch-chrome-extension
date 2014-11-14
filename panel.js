49d703cafc564031f2978190ae20ea3273369e8c
Igor Agarlev <agarlev@gmail.com>
2014-11-14 15:05:59 +0300

    add 'copy' button for request and response

--- panel.js
+++ panel.js
@@ -6,20 +6,28 @@
     var TABLE_PLACEHOLDER = '<tr><td></td><td></td><td></td></tr>'; // Placeholder for drawning vertical lines
     var BLA_BATCH_NAME = 'bla-batch';
     var RESPONSE_MAX_LENGTH = 1000;
+    var COPY_BUTTON_TEMPLATE = '<button class="copy">copy</button>';
     var METHOD_VIEW_TEMPLATE = [
         '<tr class="{{className}}">',
             '<td>',
                 '<a href="{{url}}" title="{{url}}" target="_blank">{{method}}</a>',
             '</td>',
-            '<td>{{request}}</td>',
-            '<td>{{response}}</td>',
+            '<td><span>{{request}}</span>' + COPY_BUTTON_TEMPLATE + '</td>',
+            '<td><span class="hidden">{{full-response}}</span>{{response}}' + COPY_BUTTON_TEMPLATE + '</td>',
         '</tr>'
     ].join('');
 
     // Set listeners
     clearButton.addEventListener('click', onClearButtonClick);
+    list.addEventListener('click', onListClick);
     chrome.devtools.network.onRequestFinished.addListener(onRequestFinished);
     onClearButtonClick();
+
+    function onListClick(event) {
+        if (event.target.classList.contains('copy')) {
+            copyTextToClipboard(event.target.parentNode.firstChild.innerHTML);
+        }
+    }
 
     /**
      * Clears request table.
@@ -114,11 +122,25 @@
      * @returns {String}
      */
     function renderListItem(data) {
+        var response = JSON.stringify(data.response);
         return METHOD_VIEW_TEMPLATE
             .replace('{{className}}', data.className)
             .replace('{{method}}', data.method)
-            .replace('{{url}}', data.url, 'g')
-            .replace('{{request}}', data.request ? JSON.stringify(data.request) : '')
-            .replace('{{response}}', JSON.stringify(data.response).slice(0, RESPONSE_MAX_LENGTH));
+            .replace(/\{\{url\}\}/g, data.url)
+            .replace('{{request}}', data.request? JSON.stringify(data.request) : '')
+            .replace('{{response}}', response.slice(0, RESPONSE_MAX_LENGTH))
+            .replace('{{full-response}}', response);
+    }
+
+    /**
+     * @param {String} text
+     */
+    function copyTextToClipboard(text) {
+        var textarea = document.createElement('textarea');
+        document.body.appendChild(textarea);
+        textarea.value = text;
+        textarea.select();
+        document.execCommand('copy', true);
+        document.body.removeChild(textarea);
     }
 })();
